c33bc3ac640b4ab460fc7424cb70d92f844fe553
fagnersro <fagner.hg40@gmail.com>
2023-06-29 13:12:32 -0300

    Finishing data fetches with nextJS

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,9 +3,7 @@ import styles from './page.module.css'
 import { notFound } from 'next/navigation'
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    cache: 'no-store',
-  })
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`)
 
   if (!res.ok) {
     return notFound()
@@ -22,65 +20,24 @@ export default async function BlogPost({ params }) {
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>{data.body}</p>
+          <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/259984/pexels-photo-259984.jpeg"
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Fagnersro</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/259984/pexels-photo-259984.jpeg"
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
+          <Image src={data.img} alt="" fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
-          praesentium voluptas dolore sequi, quos possimus tempore quisquam
-          autem provident commodi totam quibusdam voluptatibus doloremque velit
-          odio expedita eum, eligendi corrupti.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-          voluptatem iure quidem, veniam veritatis eligendi velit, vel cumque
-          blanditiis voluptas non fugiat amet accusamus ab cum minus saepe
-          facere eos!
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit fuga,
-          ipsa temporibus cum minus maxime dolor accusamus qui, rem consequuntur
-          asperiores voluptatem reiciendis iste quasi accusantium nesciunt. Est
-          nisi iusto laudantium aspernatur iste, dignissimos unde debitis
-          exercitationem nihil, deserunt expedita, illo voluptatum? Voluptates
-          labore odit excepturi impedit sit? Maiores placeat voluptas dolorem.
-          Ducimus provident eius dolores sapiente repellendus possimus. Aperiam
-          consequatur illo esse dolor asperiores ea accusamus impedit vel quo
-          laborum? A nam pariatur atque ut assumenda aut qui fuga dicta natus
-          iusto reprehenderit sapiente eligendi, quis voluptate exercitationem
-          reiciendis quisquam, quas odit possimus? Placeat a eaque suscipit cum
-          velit, aliquam quidem, quasi, consequatur natus facilis consectetur
-          aspernatur ducimus numquam temporibus soluta voluptas alias
-          repellendus! Velit odit quod, vel eaque deserunt exercitationem
-          tenetur sequi fugit a aliquam aut. Facilis culpa ducimus numquam, odio
-          porro officiis maiores corrupti! Quos eaque id quis magni ad veritatis
-          officia mollitia autem! Assumenda, officia repellendus. Quisquam, rem
-          quibusdam? Voluptatem explicabo quisquam cupiditate obcaecati
-          accusamus quasi modi numquam nemo, sit dignissimos repellendus dolorum
-          commodi consectetur praesentium, vero repudiandae laudantium? Quia id
-          natus provident alias at quas praesentium molestias officiis iure,
-          quasi reiciendis quidem maiores obcaecati eos, beatae atque sunt,
-          velit dignissimos. Doloremque veritatis hic corrupti consectetur.
-        </p>
+        <p className={styles.text}>{data.content}</p>
       </div>
     </div>
   )
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
 async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  const res = await fetch('http://localhost:3000/api/posts', {
     cache: 'no-store',
   })
 
@@ -23,14 +23,14 @@ export default async function Blog() {
       {data.map((item) => {
         return (
           <Link
-            href={`/blog/${item.id}`}
+            href={`/blog/${item._id}`}
             className={styles.container}
             key={item.id}
             passHref
           >
             <div className={styles.imageContainer}>
               <Image
-                src="https://images.pexels.com/photos/246658/pexels-photo-246658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                src={item.img}
                 alt=""
                 width={400}
                 height={250}
@@ -39,7 +39,7 @@ export default async function Blog() {
             </div>
             <div className={styles.content}>
               <h1 className={styles.title}>{item.title}</h1>
-              <p className={styles.desc}>{item.body}</p>
+              <p className={styles.desc}>{item.desc}</p>
             </div>
           </Link>
         )
