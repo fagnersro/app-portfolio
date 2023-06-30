fc0bad3ec696a252fb9210942fae89de0816a2c1
fagnersro <fagner.hg40@gmail.com>
2023-06-30 01:36:55 -0300

    dashboard / dashboard router

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
-import connect from '@/utils/db'
-import Post from '@/models/Post'
+import connect from '../../../../utils/db'
+import Post from '../../../../models/Post'
 
 export async function GET(request, { params }) {
   const { id } = params
@@ -10,6 +10,19 @@ export async function GET(request, { params }) {
     const post = await Post.findById(id)
 
     return new NextResponse(JSON.stringify(post), { status: 200 })
+  } catch (err) {
+    return new NextResponse('Database Error', { status: 500 })
+  }
+}
+
+export async function DELETE(request, { params }) {
+  const { id } = params
+
+  try {
+    await connect()
+    await Post.findByIdAndDelete(id)
+
+    return new NextResponse('Post has been Deleted', { status: 200 })
   } catch (err) {
     return new NextResponse('Database Error', { status: 500 })
   }
